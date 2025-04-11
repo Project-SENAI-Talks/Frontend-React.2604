88fee93a5efc7b05820b891035d54a09e8ecb4b7
luizbrito7 <luizbrito.x@gmail.com>
2025-04-11 20:46:06 -0300

    feat: add controler import (pages and components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
- 
+import { Button } from "@/components/ui/controler"
+import { Home } from "@/components/pages/controler" 
+
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
+      <Home />
     </div>
   )
 }
